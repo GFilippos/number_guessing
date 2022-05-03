3a7b663f25b345e87cfac3e64f7978fc6c873136
GFilippos <gomelisfil@gmail.com>
2022-05-03 16:10:52 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// document.querySelector('.message').textContent =
+// '🎉Correct Number!'
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+const x = () => console.log(23)
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if(!guess) {
+
+    }
+})
